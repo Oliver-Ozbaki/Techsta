7f04384b277802b1b9f05c1eabc7eec374c4e191
Oliver Ozbaki <oliver.ozbaki@outlook.com>
2024-08-15 14:57:22 +0200

    Setup environment

---
+++ README.md
@@ -0,0 +1,15 @@
+# # TECHSTA - Enchance computing perfomance.
+
+This is a client-side application that provides listings of computer parts based besides post addition for the apps blog.
+
+[Go ahead, check it out.]()
+
+## Application features.
+
+### Main.
+
+-
+
+### Aside.
+
+-
---
+++ src/router/Router.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/home/Home";
+import Blog from "../pages/blog/Blog";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
